96d6aa4421eff7a8af2319dcedf39e9c46219b5a
IgnacioCamIMT <43856051+IgnacioCamIMT@users.noreply.github.com>
2023-11-03 15:29:58 -0400

    Add files via upload
    Algoritmo de ecuación general

---
+++ algoritmo/Program.cs
@@ -0,0 +1,6 @@
+﻿static double algoritmo(double a, double b, double c) {
+    return (-b + Math.Sqrt((b * b) - (4 * a * c))) / (2 * a);
+}
+
+double resultado = algoritmo(1, 4, 2);
+Console.WriteLine(resultado);
